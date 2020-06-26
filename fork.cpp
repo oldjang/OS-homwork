2fc8b5f0f01462430fadbaa2bce18bd34ec368c8
oldjang <30384808+oldjang@users.noreply.github.com>
2020-06-26 18:37:06 +0800

    添加注释

--- fork.cpp
+++ fork.cpp
@@ -5,18 +5,18 @@ using namespace std;
 
 int main()
 {
-    pid_t fpid;
-    char ch='A';
-    for(int i=0;i<2;i++)
+    pid_t fpid; //进程ID
+    char ch='A';  //标识进程ABC
+    for(int i=0;i<2;i++)//创建两个子进程
     {
         fpid=fork();
         if(fpid==-1)
-            cout<<"ERROR"<<endl;
+            cout<<"ERROR"<<endl; //如果创建进程错误
         if(fpid==0){
-            ch=ch+i+1;
+            ch=ch+i+1; //如果是子进程，那么用ch标识不同进程
             break;
         }
     }
-    cout<<"The process "<<ch<<" is running!"<<endl;
+    cout<<"The process "<<ch<<" is running!"<<endl; //输出进程ABC
     return 0;
 }
