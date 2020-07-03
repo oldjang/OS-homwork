f28dd52354a8f75a3b25e42722943857f8e0dc84
oldjang <oldjang@outlook.com>
2020-07-03 17:14:29 +0800

    down

--- Banker's Algorithm.cpp
+++ Banker's Algorithm.cpp
@@ -4,25 +4,33 @@
 using namespace std;
 
 //重载vector的加减法
-vector<int> operator+(const vector<int>& v1, const vector<int>& v2) {
-	
-  	vector<int> r;
-  	r.reserve(v1.size());
-  	for(auto i=0; i<v1.size();i++) 
-    	r.push_back(v1[i] + v2[i]);
-  	return r;
+vector<int> operator+(const vector<int> &v1, const vector<int> &v2)
+{
+
+    vector<int> r;
+    for (auto i = 0; i < v1.size(); i++)
+        r.push_back(v1[i] + v2[i]);
+    return r;
 }
 
-vector<int> operator-(const vector<int>& v1, const vector<int>& v2) {
-	
-  	vector<int> r;
-  	r.reserve(v1.size());
-  	for(auto i=0; i<v1.size();i++) 
-    	r.push_back(v1[i] - v2[i]);
-  	return r;
+vector<int> operator-(const vector<int> &v1, const vector<int> &v2)
+{
+
+    vector<int> r;
+    for (auto i = 0; i < v1.size(); i++)
+        r.push_back(v1[i] - v2[i]);
+    return r;
 }
 
-vector<int> available, tryAvailable, request;
+bool operator<=(const vector<int> &v1, const vector<int> &v2)
+{
+    bool r = 1;
+    for (auto i = 0; i < v1.size(); i++)
+        r &= v1[i] <= v2[i];
+    return r;
+}
+
+vector<int> available, tryAvailable;
 
 struct Process
 {
@@ -39,8 +47,6 @@ void init() //输入初始状态
     cout << "Enter the number of resource types : ";
     cin >> resourceType;
     available.resize(resourceType);
-    tryAvailable.resize(resourceType);
-    request.resize(resourceType);
 
     //输入初始资源的数量
     cout << "Enter the number of each resource : ";
@@ -51,7 +57,6 @@ void init() //输入初始状态
     cout << "Enter the number of processes: ";
     cin >> processNum;
     process.resize(processNum);
-    tryProcess.resize(processNum);
 
     //输入每个进程需要的资源
     cout << "Enter the resources required by each process : " << endl;
@@ -59,27 +64,53 @@ void init() //输入初始状态
     {
         cout << "Enter the number of each resource required by the " << i << "-th process : ";
         process[i].need.resize(resourceType);
-        tryProcess[i].need.resize(resourceType);
         for (int j = 0; j < resourceType; j++)
             cin >> process[i].need[j];
-        process[i].allocation.resize(resourceType);
-        tryProcess[i].allocation.resize(resourceType);
     }
 
     //如果有需要，输入已经分配的资源情况
+    cout << "Do you need to allocate resources in advance?(y:yes,n:no)" << endl;
+    for (int i = 0; i < processNum; i++)
+        process[i].allocation.resize(resourceType);
+    char yOrn;
+    cin >> yOrn;
+
+    if (yOrn == 'y')
+    {
+        for (int i = 0; i < processNum; i++)
+        {
+            cout << "Enter the number of each resource allocated to the " << i << "-th process : ";
+            for (int j = 0; j < resourceType; j++)
+                cin >> process[i].allocation[j];
+        }
+    }
 }
 
-bool isSecure()
+vector<int> findSafeSequence() //安全性检查
 {
-    //安全性检查
-    //输出安全序列
-}
+    vector<int> nowAvailable = available;
+    vector<bool> released(false, processNum);
+    vector<int> safeSequence;
 
-bool tryAllocate() //尝试分配
-{
-    //复制向量
-    //分配
-    //安全性检查
+    for (int i = 0; i < processNum; i++)
+    {
+        bool flag = 0;
+        for (int j = 0; j < processNum; j++)
+            if (process[j].need <= nowAvailable)
+            {
+                released[j] = true;
+                nowAvailable = nowAvailable + process[j].allocation;
+                safeSequence.push_back(j);
+                flag = 1;
+                break;
+            }
+        if (!flag)
+        {
+            safeSequence.clear();
+            break;
+        }
+    }
+    return safeSequence;
 }
 
 int main()
@@ -88,14 +119,68 @@ int main()
 
     while (true)
     {
+        cout << "Now the need matrix is:" << endl;
+        for (int i = 0; i < processNum; i++)
+        {
+            for (int j = 0; j < resourceType; j++)
+                cout << process[i].need[j] << ' ';
+            cout << endl;
+        }
+
+        cout << "Now the available vector is:" << endl;
+        for (int i = 0; i < resourceType; i++)
+            cout << available[i] << ' ';
+        cout << endl;
+
         int pid;
         cout << "Enter the pid: ";
         cin >> pid;
         cout << "Enter the resource vector of process " << pid << " : " << endl;
+
         //输入请求向量
-        //假如可以分配，分配向量，输出目前状态
+        vector<int> request;
+        request.resize(resourceType);
+        for (int i = 0; i < resourceType; i++)
+            cin >> request[i];
+
         //如果请求向量大于所需资源向量，输出错误信息
+        if (!(request <= process[pid].need))
+        {
+            cout << "Request exceeds required resources!" << endl;
+            continue;
+        }
+
         //如果请求向量大于当前可利用资源向量，则让进程等待
+        if (!(request <= available))
+        {
+            cout << "Request is greater than available resources!" << endl;
+            continue;
+        }
+
+        //假如可以分配
+
+        //尝试分配
+        process[pid].allocation = process[pid].allocation + request;
+        process[pid].need = process[pid].need - request;
+        available = available - request;
+
+        //检查是否安全
+        vector<int> safeSequece = findSafeSequence();
+        if (safeSequece.size() > 0)
+        {
+            cout << "Successful request!" << endl;
+            cout << "The current safety sequence can be: ";
+            for (int i = 0; i < processNum; i++)
+                cout << safeSequece[i] << ' ';
+            cout << endl;
+        }
+        else
+        {
+            cout << "Request failed!" << endl;
+            process[pid].allocation = process[pid].allocation - request;
+            process[pid].need = process[pid].need + request;
+            available = available + request;
+        }
     }
 
     return 0;
